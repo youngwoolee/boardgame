bfe21e8b1dbb2cddd9cc9745dacb3952fadbd640
joeylee <leeyw910205@gmail.com>
2025-07-11 20:43:18 +0900

    feat: 프론트엔드 설정 파일 추가

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import {Routes, Route} from "react-router-dom";
+import SignUp from "./views/Authentication/SignUp";
+import SignIn from "./views/Authentication/SignIn";
+import OAuth from "./views/Authentication/OAuth";
+import Main from "./views/Main";
+
+function App() {
+
+  return (
+    <Routes>
+        <Route path="/game" element={<Main />} />  {/* ✅ 메인페이지 추가 */}
+        <Route path={'/auth'}>
+            <Route path={'sign-up'} element={<SignUp />} />
+            <Route path={'sign-in'} element={<SignIn />} />
+            <Route path={'oauth-response/:token/:expirationTime'} element={<OAuth />} />
+        </Route>
+    </Routes>
+  );
+}
+
+export default App;
